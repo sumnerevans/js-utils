743deb2695eea70188af8934f7e7aad83fdef702
Sumner Evans <sumner.evans98@gmail.com>
2017-06-10 19:47:59 -0600

    added sort-by, enumerate to Array; added ord to String

--- String.js
+++ String.js
@@ -1,16 +1,16 @@
 define(function() {
   /**
-   * Returns the maximum element in the array.
+   * Formats a string given a set of fomat arguments.
    *
-   * @example 'My name is {0}. Welcome to {1}.'.format('Bob', 'Denver');
+   * @example string.format('My name is {0}. Welcome to {1}.', 'Bob', 'Denver');
    *          => 'My name is Bob. Welcome to Denver.'
    *
-   * @param {function} [evaluator] the function to use to evaluate the value of
-   *                               each element of the array.
-   * @returns the maximum element in the array
+   * @param {string} str the format string
+   * @param {...} ...formatArgs the arguments to use to format the string
+   * @returns {string} the formatted string
    */
-  let format = function(string, ...formatArgs) {
-    let formattedString = string;
+  let format = function(str, ...formatArgs) {
+    let formattedString = str;
     for (let i = 0; i < formatArgs.length; i++) {
       let reg = new RegExp('\\{' + i + '\\}', 'gm');
 
@@ -25,12 +25,44 @@ define(function() {
     return formattedString;
   };
 
+  /**
+   * Get the ordinal (ASCII) value of the given character.
+   *
+   * @param {string} str the char to get the ordinal value of.
+   * @returns {number} the ordinal value of the character.
+   */
+  let ord = function(str) {
+    if (str.length !== 1) {
+      throw 'String.ord expected a string of length 1.';
+    }
+    return str.charCodeAt(0);
+  };
+
   // Add the functions to the String prototype.
+  /**
+   * Formats a string given a set of fomat arguments.
+   *
+   * @example 'My name is {0}. Welcome to {1}.'.format('Bob', 'Denver');
+   *          => 'My name is Bob. Welcome to Denver.'
+   *
+   * @param {...} ...formatArgs the arguments to use to format the string
+   * @returns {string} the formatted string
+   */
   String.prototype.format = String.prototype.format || function(...formatArgs) {
     return format(this, ...formatArgs);
   };
 
+  /**
+   * Get the ordinal (ASCII) value of the given character.
+   *
+   * @returns {number} the ordinal value of the character.
+   */
+  String.prototype.ord = String.prototype.ord || function() {
+    return ord(this);
+  };
+
   return {
-    'format': format,
+    format: format,
+    ord: ord,
   };
 });
