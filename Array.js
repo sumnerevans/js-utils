10827cf0c8b2765ab10ded99211b7dee7a1edf21
Sumner Evans <sumner.evans98@gmail.com>
2017-06-27 22:02:07 -0600

    more cool array utils

--- Array.js
+++ Array.js
@@ -1,5 +1,18 @@
 define(() => {
   /**
+   * Enumerate the array with the pair [index, value].
+   *
+   * @param {array} array the array to enumerate
+   * @yields an array with the index of the element and the element itself for
+   *         each element of the original array.
+   */
+  let enumerate = function*(array) {
+    for (let i = 0; i < array.length; i++) {
+      yield [i, array[i]];
+    }
+  };
+
+  /**
    * Returns the maximum element in the array.
    *
    * @param {array}    array       the array to find the minimum on
@@ -58,11 +71,56 @@ define(() => {
   };
 
   /**
+   * Creates a generator that yields elements between the start and the stop
+   * with the given step.
+   *
+   * @example array.range(10) => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+   * @example array.range(3, 10) => [3, 4, 5, 6, 7, 8, 9]
+   * @example array.range(3, 10, 2) => [3, 5, 7, 9]
+   *
+   * @param {int} start/stop the beginning of the range, or if this is the only
+   *                         parameter, then the end of the range
+   * @param {int} stop the end of the range (not inclusive)
+   * @param {int} step the step to increment by
+   */
+  let range = function*(start, stop, step) {
+    if (stop === undefined) {
+      stop = start;
+      start = 0
+    }
+
+    if (step === undefined) {
+      step = 1;
+    }
+
+    for (let i = start; i < stop; i += step) {
+      yield i;
+    }
+
+    return;
+  };
+
+  /**
+   * Removes the given indexes from the array.
+   *
+   * @param {array} array the array to remove elements from
+   * @param {int} start the first index to remove
+   * @param {int} stop the first index to remove
+   * @returns {array} the array without the elements (note, the actual array
+   *                  reference is modified)
+   */
+  let remove = function(array, start, stop) {
+    stop = stop || start;
+    array.splice(start, (stop - start) + 1);
+    return array;
+  };
+
+  /**
    * Sort the array according to the given evaluator function.
    *
    * @param {array} array the array to sort
    * @param {function} evaluator the function to use to evaluate the value of
-   *                             each element of the array.
+   *                             each element of the array
    * @returns {*} the sorted array
    */
   let sortBy = function(array, evaluator) {
@@ -75,18 +133,34 @@ define(() => {
   };
 
   /**
-   * Enumerate the array with the pair [index, value].
+   * Yields the elements of the array for which the evaluator returns true.
    *
-   * @param {array} array the array to enumerate
-   * @returns {array} yields the index and the value at that index.
+   * @param {array} array the array
+   * @param {function} evaluator=el=>el the function to evaluate the elements
+   * @yields the elements of the array for which the evaluator returns true.
    */
-  let enumerate = function*(array) {
-    for (let i = 0; i < array.length; i++) {
-      yield [i, array[i]];
+  let where = function*(array, evaluator = el => true) {
+    for (let el of array) {
+      if (evaluator(el)) {
+        yield el;
+      }
     }
   };
 
   // Add the functions to the Array prototype.
+  /**
+   * Enumerate the array with the pair [index, value].
+   *
+   * @param {array} array the array to enumerate
+   * @yields an array with the index of the element and the element itself for
+   *         each element of the original array.
+   */
+  Array.prototype.enumerate = Array.prototype.enumerate || function*() {
+    for (let el of enumerate(this)) {
+      yield el;
+    }
+  };
+
   /**
    * Returns the maximum element in the array.
    *
@@ -110,6 +184,18 @@ define(() => {
   };
 
   /**
+   * Removes the given indexes from the array.
+   *
+   * @param {int} start the first index to remove
+   * @param {int} stop the first index to remove
+   * @returns {array} the array without the elements (note, the actual array
+   *                  reference is modified)
+   */
+  Array.prototype.remove = Array.prototype.remove || function(start, stop) {
+    return remove(this, start, stop);
+  };
+
+  /**
    * Sort the array according to the given evaluator function.
    *
    * @param {function} evaluator the function to use to evaluate the value of
@@ -121,21 +207,39 @@ define(() => {
   };
 
   /**
-   * Enumerate the array with the pair [index, value].
+   * Yields the elements of the array for which the evaluator returns true.
    *
-   * @param {array} array the array to enumerate
-   * @returns {array} yields the index and the value at that index.
+   * @param {array} array the array
+   * @param {function} evaluator=el=>el the function to evaluate the elements
+   * @yields the elements of the array for which the evaluator returns true.
    */
-  Array.prototype.enumerate = Array.prototype.enumerate || function*() {
-    for (let el of enumerate(this)) {
+  Array.prototype.where = Array.prototype.where || function*(evaluator) {
+    for (let el of where(this, evaluator)) {
       yield el;
     }
   };
 
-  return {
-    enumerate: enumerate,
-    max: max,
-    min: min,
-    sortBy: sortBy,
+  /**
+   * Convert a generator or string to an array.
+   *
+   * @param {iterable} source an iterable object
+   * @returns {array} the array from the generator function
+   */
+  let array = function(source) {
+    let arr = [];
+    for (let el of source) {
+      arr.push(el);
+    }
+    return arr;
   };
+
+  array.enumerate = enumerate;
+  array.max = max;
+  array.min = min;
+  array.range = range;
+  array.remove = remove;
+  array.sortBy = sortBy;
+  array.where = where;
+
+  return array;
 });
--- test/array-test.js
+++ test/array-test.js
@@ -9,10 +9,20 @@ let array = requirejs('Array');
 requirejs('String');
 
 describe('Array', function() {
-  let intArray = [1, 6, 2, 3, 4, 5];
+  it('should convert an iterator to an array', function() {
+    let generator = function*() {
+      yield 1;
+      yield 2;
+      yield 3;
+      return;
+    }
+    assert.deepEqual([1, 2, 3], array(generator()));
+    assert.deepEqual(['t', 'e', 's', 't'], array('test'))
+  });
+
+  let intArray = [1, 2, 3, 4, 5, 6];
 
   describe('#enumerate()', function() {
-
     it('should return work in for loops', function() {
       let index = 0;
       for (let [i, el] of intArray.enumerate()) {
@@ -43,7 +53,6 @@ describe('Array', function() {
   });
 
   describe('#max()', function() {
-
     it('should return null when the array is empty', function() {
       assert.equal(null, [].max());
       assert.equal(null, array.max([], () => {}));
@@ -84,13 +93,58 @@ describe('Array', function() {
       });
   });
 
+  describe('#range()', function() {
+    it('should generate a range between [0, start)', function() {
+      assert.deepEqual([0, 1, 2, 3, 4], array(array.range(5)));
+      assert.deepEqual([], array(array.range(0)));
+    });
+
+    it('should generate a range between [start, stop)', function() {
+      assert.deepEqual([1, 2, 3, 4], array(array.range(1, 5)));
+      assert.deepEqual([3, 4, 5, 6], array(array.range(3, 7)));
+    });
+
+    it('should generate a range between [start, stop) with the step',
+      function() {
+        assert.deepEqual([1, 3, 5], array(array.range(1, 6, 2)));
+        assert.deepEqual([3, 6, 9], array(array.range(3, 10, 3)));
+      });
+  });
+
+  describe('#remove()', function() {
+    it('should remove a single item', function() {
+      let arr = [1, 2, 3, 4, 5];
+      array.remove(arr, 2)
+      assert.deepEqual([1, 2, 4, 5], arr);
+
+      arr = [1, 2, 3, 4, 5];
+      arr.remove(3);
+      assert.deepEqual([1, 2, 3, 5], arr);
+
+      arr = [1, 2, 3, 4, 5];
+      assert.deepEqual([1, 2, 3, 5], arr.remove(3));
+    });
+
+    it('should remove a range of items', function() {
+      let arr = [1, 2, 3, 4, 5];
+      array.remove(arr, 0, 2)
+      assert.deepEqual([4, 5], arr);
+
+      arr = [1, 2, 3, 4, 5];
+      arr.remove(1, 3);
+      assert.deepEqual([1, 5], arr);
+
+      arr = [1, 2, 3, 4, 5];
+      assert.deepEqual([1, 5], arr.remove(1, 3));
+    });
+  });
+
   describe('#sortBy()', function() {
     it('should return a sorted array according to the given lambda',
       function() {
         assert.deepEqual([1, 2, 3], [3, 1, 2].sortBy(x => x));
         assert.deepEqual([1, 2, 3], array.sortBy([3, 1, 2], x => x));
-        assert.deepEqual(['a', 'b', 'c'], ['c', 'a', 'b'].sortBy(x =>
-          x.ord()));
+        assert.deepEqual(['a', 'b', 'c'], ['c', 'a', 'b'].sortBy(x => x.ord()));
         assert.deepEqual([{
           a: 2
         }, {
@@ -104,6 +158,42 @@ describe('Array', function() {
         }, {
           a: 3
         }].sortBy(x => x.a));
+      });
+  });
+
+  describe('#where()', function() {
+    it(
+      'should return a generator that yields the elements filtered by the evaluator',
+      function() {
+        let arr = [{
+          a: true,
+          id: 1,
+        }, {
+          b: true,
+          id: 2,
+        }, {
+          a: true,
+          id: 3,
+        }];
+
+        assert.deepEqual([{
+          a: true,
+          id: 1,
+        }, {
+          a: true,
+          id: 3,
+        }], array(array.where(arr, el => el.a)));
+
+        assert.deepEqual([{
+          a: true,
+          id: 1,
+        }, {
+          b: true,
+          id: 2,
+        }], array(array.where(arr, el => el.id < 3)));
+
+        let numbers = array(array.range(3, 10));
+        assert.deepEqual([5, 6, 7], array(numbers.where(x => x > 4 && x < 8)));
       });
   });
 });
